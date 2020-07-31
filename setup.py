7d4546d59e0e39e7e50faa7b5c2197d0911bd870
Janis Klaise <jk@seldon.io>
2020-07-31 15:58:27 +0100

    Add dependencies for the data loading functions

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=['tensorflow>=2.0.0'],
+    install_requires=[
+        'alibi',
+        'numpy',
+        'scikit-learn',
+        'tensorflow>=2.0.0'
+        ],
     package_data={'': ['models/*']}
 )
