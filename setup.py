dfc4e50d1763577e6b231a3b06eaecfa63496827
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-05-17 15:39:19 +0100

    Update tensorflow and scikit-learn upper bounds

--- setup.py
+++ setup.py
@@ -15,15 +15,15 @@ model_files = package_files('alibi_testing/models/')
 
 setup(
     name='alibi-testing',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages(),
     python_requires='>=3.7',
     install_requires=[  # deps version bounds are copied from alibi setup.py (and requirements/dev.txt)
         'numpy>=1.16.2, <2.0.0',
         'pandas>=0.23.3, <2.0.0',
         'requests>=2.21.0, <3.0.0',
-        'scikit-learn>=0.20.2, <1.1.0',
-        'tensorflow>=2.0.0, !=2.6.0, !=2.6.1, <2.9.0',  
+        'scikit-learn>=0.20.2, <2.0.0',
+        'tensorflow>=2.0.0, !=2.6.0, !=2.6.1, <2.10.0',  
         'torch>=1.9.0, <2.0.0',
         'torchvision>=0.10.0, <1.0.0'
     ],
