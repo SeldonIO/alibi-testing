c5c647906c89328634afceab7f9ad6d3c2afbd4c
Robert <robert.samoilescu@gmail.com>
2024-12-05 12:47:31 +0000

    Refactored train mnist pt

--- scripts/train_mnist_pt.py
+++ scripts/train_mnist_pt.py
@@ -4,21 +4,24 @@ import torch.nn.functional as F
 import torchvision
 
 from alibi_testing.modules import CNN
+from utils import save_model
 
 
 def load_data():
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-    mnist_train = torchvision.datasets.MNIST(root='datasets',
-                                             train=True,
-                                             download=True,
-                                             transform=transform)
-    mnist_test = torchvision.datasets.MNIST(root='datasets',
-                                            train=False,
-                                            transform=transform)
-
+    mnist_train = torchvision.datasets.MNIST(
+        root='datasets',
+        train=True,
+        download=True,
+        transform=transform
+    )
+    mnist_test = torchvision.datasets.MNIST(
+        root='datasets',
+        train=False,
+        transform=transform
+    )
     train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=64, shuffle=True)
     test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=64, shuffle=True)
-
     return train_loader, test_loader
 
 
@@ -32,11 +35,15 @@ def train(model, optimizer, train_loader, epoch, log_interval=100):
         optimizer.step()
 
         if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,
-                                                                           batch_idx * len(data),
-                                                                           len(train_loader.dataset),
-                                                                           100. * batch_idx / len(train_loader),
-                                                                           loss.item()))
+            print(
+                'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader),
+                    loss.item()
+                )
+            )
 
 
 def test(model, test_loader):
@@ -49,20 +56,23 @@ def test(model, test_loader):
             test_loss += F.cross_entropy(out, target, reduce=None).item()
             pred = out.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
+
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss,
-                                                                              correct,
-                                                                              len(test_loader.dataset),
-                                                                              100. * correct / len(
-                                                                                  test_loader.dataset)))
+    print(
+        '\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)
+        )
+    )
 
 
-def run_model():
+def run_model(n_epochs = 1):
     train_loader, test_loader = load_data()
+
     model = CNN()
     optimizer = torch.optim.Adam(model.parameters())
-
-    n_epochs = 3
 
     for epoch in range(1, n_epochs + 1):
         train(model, optimizer, train_loader, epoch)
@@ -73,8 +83,19 @@ def run_model():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', type=str, help='Name of the model to be saved')
+    parser.add_argument('--model', type=str, choices=["cnn"], default="cnn", help='Name of the model to be saved')
+    parser.add_argument('--format', type=str, choices=["pt"], default="pt")
     args = parser.parse_args()
 
+    # train model
     model = run_model()
-    torch.save(model, args.name)
+
+    # save trained mode
+    save_model(
+        model,
+        args,
+        model_name=args.model,
+        data="mnist",
+        framework="pt",
+        version=torch.__version__.split("+")[0]
+    )
